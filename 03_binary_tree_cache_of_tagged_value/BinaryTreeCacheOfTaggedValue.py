f6b676393f293d9df32f9d35270bec38b2fa3fc1
David SPORN <sporniket.studio@gmail.com>
2022-04-21 08:37:46 +0200

    no more errors, can run a few cycles

--- 03_binary_tree_cache_of_tagged_value/BinaryTreeCacheOfTaggedValue.py
+++ 03_binary_tree_cache_of_tagged_value/BinaryTreeCacheOfTaggedValue.py
@@ -1,3 +1,5 @@
+### local deps
+from CellOfTaggedValue import CellOfTaggedValue
 ### main deps
 from amaranth import *
 from amaranth.build import Platform
@@ -16,7 +18,7 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
     A new value can be added to the set. When the set is full, a value that has not been recently in use is replaced.
     """
 
-    def __init__(self, level: int, tagSeed:int, tagWidth:int, valueShape:Shape, isRoot:bool = False):
+    def __init__(self, level: int, tagSeed:int, tagWidth:int, valueShape:Shape, isRoot:bool = True):
         if isRoot and tagWidth <= level:
             raise ValueError('tagWidth should be strictly superior to root level value')
         # Store parameters to instanciate submodules inside elaborate
@@ -28,15 +30,16 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
 
         # Registers
         self.oldest = Signal() # 0 -> left branch/leaf ; 1 -> right branch/leaf
+        self.needChangeOldest = Signal()
 
         # inputs
         self.writeEnabled = Signal() # should be asserted to bind value in dataIn to the tag.
         self.dataIn = Signal(shape=valueShape, reset_less=True) # the value to compare to internal value, or the value to bind to the tag.
 
         #outputs
-        self.isMatching = Signal(reset_less=True) # asserted when one of the leaf is binded to dataIn.
+        self.isMatching = Signal() # asserted when one of the leaf is binded to dataIn.
         self.hasFreeTag = Signal(reset_less=True) # asserted when no value has been bound to the tag
-        self.dataOut = Signal(shape=tagShape,reset_less=True) # the tag.
+        self.dataOut = Signal(shape=unsigned(tagWidth),reset_less=True) # the tag.
 
     def ports(self) -> List[Signal]:
         return [
@@ -48,10 +51,10 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
         ]
 
     def elaborate(self, platform: Platform) -> Module:
-        if 0 < self.level:
-            return elaborate_tree(self, platform)
-        else
-            return elaborate_tree(self, platform)
+        if 0 == self.level:
+            return self.elaborate_leaf(platform)
+        else:
+            return self.elaborate_tree(platform)
 
     def elaborate_leaf(self, platform: Platform) -> Module:
         m = Module()
@@ -61,7 +64,7 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
         m.submodules.left = left = CellOfTaggedValue(subTagSeed, unsigned(self.tagWidth), self.valueShape)
         m.submodules.right = right = CellOfTaggedValue(subTagSeed, unsigned(self.tagWidth), self.valueShape)
 
-        needChangeOldest = Signal
+        needChangeOldest = Signal()
 
         # wire everything
         # -- asynchronous logic
@@ -74,11 +77,10 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
             # outputs
             self.isMatching.eq(left.isMatching | right.isMatching),
             self.hasFreeTag.eq(left.isFree | right.isFree),
-            self.dataOut.eq(Mux(left.isMatching,left.dataOut, right.dataOut))
+            self.dataOut.eq(Mux(left.isMatching,left.dataOut, right.dataOut)),
             # maintain 'oldest'
-            needChangeOldest.eq((~(self.oldest) & (left.isMatching)) # either the oldest was on the left and it is now matching
+            self.needChangeOldest.eq((~(self.oldest) & (left.isMatching)) # either the oldest was on the left and it is now matching
                 | ((self.oldest) & (right.isMatching))) # or the oldest was on the right and it is now matching
-            self.oldest.eq(Mux(needChangeOldest), ~(self.oldest), self.oldest)
         ]
         if self.isRoot:
             # the root protect the whole tree against duplicate binding of a value
@@ -94,6 +96,10 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
                 right.writeEnabled.eq(self.writeEnabled)
             ]
 
+        m.d.sync += [
+            self.oldest.eq(Mux(needChangeOldest, ~(self.oldest), self.oldest))
+        ]
+
         return m
 
     def elaborate_tree(self, platform: Platform) -> Module:
@@ -102,10 +108,10 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
         # instanciate submodules
         subLevel = self.level - 1
         subTagSeed = 2 * self.tagSeed
-        m.submodules.left = left = BinaryTreeCacheOfTaggedValue(subLevel, subTagSeed, self.tagWidth, self.valueShape)
-        m.submodules.right = right = BinaryTreeCacheOfTaggedValue(subLevel, subTagSeed + 1, self.tagWidth, self.valueShape)
+        m.submodules.left = left = BinaryTreeCacheOfTaggedValue(subLevel, subTagSeed, self.tagWidth, self.valueShape, isRoot=False)
+        m.submodules.right = right = BinaryTreeCacheOfTaggedValue(subLevel, subTagSeed + 1, self.tagWidth, self.valueShape, isRoot=False)
 
-        needChangeOldest = Signal
+        needChangeOldest = Signal()
 
         # wire everything
         # -- asynchronous logic
@@ -118,11 +124,11 @@ class BinaryTreeCacheOfTaggedValue(Elaboratable):
             # outputs
             self.isMatching.eq(left.isMatching | right.isMatching),
             self.hasFreeTag.eq(left.hasFreeTag | right.hasFreeTag),
-            self.dataOut.eq(Mux(left.isMatching,left.dataOut, right.dataOut))
+            self.dataOut.eq(Mux(left.isMatching,left.dataOut, right.dataOut)),
             # maintain 'oldest'
             needChangeOldest.eq((~(self.oldest) & (left.isMatching)) # either the oldest was on the left and it is now matching
-                | ((self.oldest) & (right.isMatching))) # or the oldest was on the right and it is now matching
-            self.oldest.eq(Mux(needChangeOldest), ~(self.oldest), self.oldest)
+                | ((self.oldest) & (right.isMatching))),
+            self.oldest.eq(Mux(needChangeOldest, ~(self.oldest), self.oldest))
         ]
         if self.isRoot:
             # the root protect the whole tree against duplicate binding of a value
@@ -147,24 +153,57 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Prepare : prepare the test bench
+    dataInShape = unsigned(4)
+    tagWidth = 2
     m = Module()
-    m.submodules.dut = dut = BinaryTreeCacheOfTaggedValue()
+    m.submodules.dut = dut = BinaryTreeCacheOfTaggedValue(1, 0, 2, dataInShape)
 
-    # Prepare : prepare the test bench : workaround sim bug , override clk and rst
+    # Prepare : prepare the test bench : workaround sim , override clk and rst
     nameOfClockDomain = "sync"
     m.domains.sync = sync = ClockDomain(nameOfClockDomain)
     syncClk = ClockSignal(nameOfClockDomain)
     rst = Signal()
     sync.rst = rst
+    # Prepare : prepare the test bench : workaround sim , input signals of interest
+    dataIn = Signal(dataInShape, reset=0)
+    m.d.comb += dut.dataIn.eq(dataIn)
+    writeEnabled = Signal()
+    m.d.comb += dut.writeEnabled.eq(writeEnabled)
 
     # To verify
     def myVerification(m:Module):
         pass
 
     def mySimulation(sim:Simulator, m:Module):
-
+        # !!! limit the number of clockcycle !
+        # python3 BinaryTreeCacheOfTaggedValue.py simulate -v test.vcd -w test.gtkw -c 5 
         def process():
-            pass
+            print("Start !")
+            # fill 3 slots
+            yield dataIn.eq(1)
+            yield writeEnabled.eq(1)
+            yield
+            yield dataIn.eq(2)
+            yield
+            yield dataIn.eq(4)
+            yield
+            print("Done filling 3 slots")
+            # match the oldest should be the right hand side, make a match to swap to the left
+            yield writeEnabled.eq(0)
+            yield dataIn.eq(2)
+            yield
+            print("Done activating a cell")
+            # fill a slot will do that in the left hand side
+            yield dataIn.eq(3)
+            print("beep")
+            yield writeEnabled.eq(1)
+            print("beep")
+            yield
+            print("beep")
+            yield writeEnabled.eq(0)
+            print("beep")
+            #yield
+            print("All done !")
 
         sim.add_sync_process(process)
 
