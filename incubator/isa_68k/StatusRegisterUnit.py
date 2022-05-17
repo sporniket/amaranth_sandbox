99ac90d1f77d88848a024437c8620f4bd52a68e1
David SPORN <sporniket.studio@gmail.com>
2022-05-17 06:40:53 +0200

    incubator/isa_68k/StatusRegisterUnit.py : properties, ports

---
+++ incubator/isa_68k/StatusRegisterUnit.py
@@ -0,0 +1,89 @@
+### local deps
+### main deps
+from amaranth import *
+from amaranth.build import Platform
+from typing import List, Dict, Tuple, Optional
+### test deps ###
+
+# FIXME : enums
+# -- SYSTEM
+T1_trace_each_instruction   = 0b1000000000000000
+T0_trace_control_flow       = 0b0100000000000000
+S_supervisor                = 0b0010000000000000
+M_supervisor_main           = 0b0001000000000000
+I_interrupt_treshold_7      = 0b0000011100000000
+
+# -- USER
+X_extends   = 0b00010000
+N_negative  = 0b00001000
+Z_zero      = 0b00000100
+V_overflow  = 0b00000010
+C_carry     = 0b00000001
+
+class StatusRegisterUnit(Elaboratable):
+    """
+    The unit tasked with maintaining the status register, update the flags at each clock.
+    """
+
+    def __init__(self):
+        # internal register
+        self.value = Signal(shape=unsigned(16), reset=S_supervisor | M_supervisor_main)
+
+        # inputs
+        self.Operation = Signal(unsigned(2)) # 00=or ; 01=xor ; 10=and ; 11=set
+        self.DataIn = Signal(unsigned(16))
+        self.OperationStrobe = Signal() # if asserted, perform the operation using the data input
+        self.SystemDataStrobe = Signal # if negated, do not change the system byte
+
+        # outputs
+        # -- programmer view
+        self.StatusRegister = Signal(unsigned(16), reset_less=True, name="SR") # supervisor
+        self.ConditionCodeRegister = Signal(unsigned(16), reset_less=True, name="CCR") # user
+        # -- individual flags view
+        # -- -- system
+        self.Trace = Signal(unsigned(2), reset_less=True, name="T")
+        self.Supervisor = Signal(reset_less=True, name="S")
+        self.MainOrInterrupt = Signal(reset_less=True, name="M")
+        self.InterruptLevel = Signal(unsigned(3), reset_less=True, name="I")
+        # -- -- user
+        self.Extends = Signal(reset_less=True, name="X")
+        self.Negative = Signal(reset_less=True, name="N")
+        self.Zero = Signal(reset_less=True, name="Z")
+        self.Overflow = Signal(reset_less=True, name="V")
+        self.Carry = Signal(reset_less=True, name="C")
+
+    def ports(self) -> List[Signal]:
+        return [
+            # inputs
+            self.Operation,
+            self.DataIn,
+            self.OperationStrobe,
+            self.SystemDataStrobe,
+
+            # outputs
+            # -- programmer view
+            self.StatusRegister,
+            self.ConditionCodeRegister,
+            # -- individual flags view
+            # -- -- system
+            self.Trace,
+            self.Supervisor,
+            self.MainOrInterrupt,
+            self.InterruptLevel,
+            # -- -- user
+            self.Extends,
+            self.Negative,
+            self.Zero,
+            self.Overflow,
+            self.Carry
+        ]
+    def elaborate(self, platform: Platform) -> Module:
+        m = Module()
+
+        m.d.comb += [
+            # extract all outputs from the internal registers
+            self.StatusRegister.eq(self.value)
+            # TODO... HERE
+        ]
+
+        return m
