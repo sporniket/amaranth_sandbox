482ab96710f1b0e3e5de1805c0379985585ebd52
David SPORN <sporniket.studio@gmail.com>
2022-09-18 10:52:42 +0200

    test each pin

--- board--colorlight-i9/make.py
+++ board--colorlight-i9/make.py
@@ -1,4 +1,18 @@
 from colorlight_i9 import Colorlight_I9_V7_2_Platform
 from blinky import *
+from blinky_gpio import *
 
-Colorlight_I9_V7_2_Platform().build(Blinky(), do_program=True)
+#Colorlight_I9_V7_2_Platform().build(Blinky(), do_program=True)
+
+# list of addressable pins on the first row
+row_one = (5,7,9,11,13,17,23,25,27,29)
+print("========================[START testing connector 'p'#2 -- row 1]============================")
+for pin in row_one:
+    print(f"------------------------> Pin Under Test : #{pin}")
+    Colorlight_I9_V7_2_Platform().build(BlinkyGpio("p",2,pin), do_program=True)
+    action = input("Continue ? (y/n) :")
+    if action == "n":
+        break
+print("========================[END testing connector 'p'#2]============================")
+
+print("DONE.")
