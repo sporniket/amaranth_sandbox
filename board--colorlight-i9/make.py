ddb4ab477f3b50dc3b6448e75d80df96cf4b5462
David SPORN <sporniket.studio@gmail.com>
2022-09-25 22:48:01 +0200

    Test API a la jasmine

--- board--colorlight-i9/make.py
+++ board--colorlight-i9/make.py
@@ -1,39 +1,103 @@
 from colorlight_i9 import Colorlight_I9_V7_2_Platform
 from blinky import *
 from blinky_gpio import *
+from amaranth import Elaboratable
+from amaranth.build import Platform
+from typing import List
 
-def askContinue():
+def askContinue() -> bool:
     action = input("Continue ? (y/n) :")
-    if action == "n":
+    return action == "y"
+
+def askContinueOrDie():
+    action = input("Continue ? (y/n) :")
+    if askContinue() is not True:
         print("Bye.")
         exit()
 
+
+class TestRunner:
+    @property
+    def label(self)->str:
+        return self._label
+    
+    @property
+    def help(self):
+        self._description
+    
+    def run(self):
+        raise(RuntimeError("NOT IMPLEMENTED"))
+
+class PlatformDemoTestRunner(TestRunner):
+    def __init__(self, testLabel:str, testDescription:str, platform:Platform, testModule:Elaboratable):
+        self._label = testLabel
+        self._description = testDescription
+        self.platform = platform
+        self.module = testModule
+
+    def run(self):
+        print(f"========================[ START OF {self.label} ]============================")
+        if len(self._description) > 0:
+            print(self._description)
+        self.platform.build(self.module, do_program = True)
+        print(f"-- -- -- -- -- -- -- -- [ END OF {self.label} ] -- -- -- -- -- -- -- --")
+
+class GroupOfTests(TestRunner):
+    def __init__(self, testLabel:str, testDescription:str, tests:List[TestRunner]):
+        self._label = testLabel
+        self._description = testDescription
+        self.tests = tests
+    
+    def run(self):
+        print(f"========================[ START OF GROUP {self.label} ]============================")
+        if len(self._description) > 0:
+            print(self._description)
+        if askContinue() is True:
+            for test in self.tests:
+                test.run()
+        print(f"-- -- -- -- -- -- -- -- [ END OF GROUP {self.label} ] -- -- -- -- -- -- -- --")
+    
 if __name__ == "__main__":
-    print("========================[ START testing onboard LED ]============================")
-    Colorlight_I9_V7_2_Platform().build(Blinky(), do_program=True)
-    askContinue()
-
-    # list of addressable pins on the first row
-    row_one = (5,7,9,11,13,17,23,25,27,29)
-    row_two = (6,8,10,12,14,18,24,26,28,30)
-
-    conn_index = 2 # provision for future loop
-    print(f"========================[START testing connector 'p'#{conn_index} -- row 1]============================")
-    print(f"INSTALL test rig on first row of connector {conn_index}")
-    askContinue()
-    for pin in row_one:
-        print(f"------------------------> Pin Under Test : #{pin}")
-        Colorlight_I9_V7_2_Platform().build(BlinkyGpio("p",2,pin), do_program=True)
-        askContinue()
-
-    print(f"========================[START testing connector 'p'#{conn_index} -- row 2]============================")
-    print(f"INSTALL test rig on second row of connector {conn_index}")
-    askContinue()
-    for pin in row_two:
-        print(f"------------------------> Pin Under Test : #{pin}")
-        Colorlight_I9_V7_2_Platform().build(BlinkyGpio("p",2,pin), do_program=True)
-        askContinue()
-
-    print(f"========================[END testing connector 'p'#{conn_index}]============================")
-
+    allTheTests = GroupOfTests(
+        "Demonstration of platform 'Colorlight I9 v7.2'",
+        "Build and upload a bunch of gateware demonstrating the platform resources",
+        [
+            PlatformDemoTestRunner("testing onboard LED", "Build and upload a gateware that makes onboard LED(s) blink", Colorlight_I9_V7_2_Platform(), Blinky()),
+            GroupOfTests(
+                "Testing expansion board connectors",
+                "Test connectors p2..p6 of the expansion board needs to connect LEDs to one row of the connector, for each row",
+                [
+                    GroupOfTests(f"Connector 'p'#{conn_index}", "", [
+                        GroupOfTests(
+                            f"Testing connector 'p'#{conn_index} -- row 1", 
+                            f"INSTALL test rig on row 1 of connector {conn_index}", 
+                            [
+                                PlatformDemoTestRunner(
+                                    f"Pin Under Test : #{pin}", 
+                                    "", 
+                                    Colorlight_I9_V7_2_Platform(), 
+                                    BlinkyGpio("p",conn_index,pin)
+                                )
+                                for pin in (5,7,9,11,13,17,23,25,27,29)
+                            ]
+                        ),
+                        GroupOfTests(
+                            f"Testing connector 'p'#{conn_index} -- row 2", 
+                            f"INSTALL test rig on row 2 of connector {conn_index}", 
+                            [
+                                PlatformDemoTestRunner(
+                                    f"Pin Under Test : #{pin}", 
+                                    "", 
+                                    Colorlight_I9_V7_2_Platform(), 
+                                    BlinkyGpio("p",conn_index,pin)
+                                )
+                                for pin in (6,8,10,12,14,18,24,26,28,30)
+                            ]
+                        ),
+                    ]
+                    ) for conn_index in (2,)
+                ]
+            )
+        ]
+    ).run()
     print("ALL DONE.")
