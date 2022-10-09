5c466c75ea803ee1aaf16ed39759e8b6b8ccecec
David SPORN <sporniket.studio@gmail.com>
2022-10-09 05:43:24 +0200

    rewrite blinky to use slowbeat module

--- board--colorlight-i9/blinky.py
+++ board--colorlight-i9/blinky.py
@@ -3,6 +3,9 @@ from amaranth import *
 from amaranth.build import Platform
 from typing import List, Dict, Tuple, Optional
 
+### local deps
+from slowbeat import SlowBeat
+
 __all__ = ["Blinky"]
 
 class Blinky(Elaboratable):
@@ -14,16 +17,8 @@ class Blinky(Elaboratable):
         m = Module()
         
         led = platform.request("led",0)
+        m.submodules.beat = beat = SlowBeat(1)
 
-        clk_freq = platform.default_clk_frequency
-        timer = Signal(range(int(clk_freq//2)), reset=int(clk_freq//2) - 1)
-        blink = Signal(reset=1)
-
-        m.d.comb += led.eq(blink)
-        with m.If(timer == 0):
-            m.d.sync += timer.eq(timer.reset)
-            m.d.sync += blink.eq(~blink)
-        with m.Else():
-            m.d.sync += timer.eq(timer - 1)
+        m.d.comb += led.eq(beat.beat_p)
         
         return m
